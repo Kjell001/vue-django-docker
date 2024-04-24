6445dd624cf36db23e0d17b7152667320bf15a53
Kjell Boeije <k.boeije@repowered.nl>
2024-04-25 01:15:49 +0200

    Style fixes

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -12,9 +12,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.GROUP_NAME, self.channel_name
-        )
+        await self.channel_layer.group_discard(self.GROUP_NAME, self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -30,7 +28,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 "content": data["content"],
             },
         )
-    
+
     # Handle chat_message group event
     async def chat_message(self, event):
         await self.send(text_data=json.dumps(event))
